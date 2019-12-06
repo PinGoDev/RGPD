01cc5e5af278ccef0b44267ed72c4975d2e9f6d0
PinGoDev <pingodevweb@gmail.com>
2019-12-06 12:47:58 -0300

    inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+     <?php 
+    include_once dirname(__FILE__).'/blocks/styles.php';
+    ?>
+  </head>
+  <body>
+	  
+	<!-- BEGIN: copiar esto -->
+    <?php 
+      include_once dirname(__FILE__).'/blocks/part_1.php';
+    ?>
+    <!--  END: copiar esto -->
+
+ </body>
+</html>
+
